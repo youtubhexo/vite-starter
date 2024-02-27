3bbf7f07b341eab674bc503f2df669a518d1bf77
张王岩 <youtubhexo@gmail.com>
2024-02-27 14:56:47 +0800

    vite-plugin-pages及进阶用法

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Hello world by vite-plugin-pages
+    <img src="https://desperado.cloud/picgo-img//20240227135105.png">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
