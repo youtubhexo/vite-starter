3b8f40116fc6cc68e86019dc6bf9f16aaae9eba2
张王岩 <youtubhexo@gmail.com>
2024-03-01 16:50:07 +0800

    1、remove  vite-plugin-pages,采用 unplugin-vue-router 2、使用自动导入 unplugin-auto-import 3、引入vueuse

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,13 +3,15 @@
     <span class="text-3xl font-bold underline">
       Hello world by vite-plugin-pages
     </span>
+    <div>Now: {{ now }}</div>
 
     <!-- //<img src="https://desperado.cloud/picgo-img//20240227135105.png"> -->
   </div>
 </template>
+<script setup lang="ts">
+import { useNow } from '@vueuse/core'
 
-<script setup>
-
+const now = useNow()
 </script>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,43 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import Pages from 'vite-plugin-pages'
 import UnoCSS from 'unocss/vite'
+
+import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), Pages(), UnoCSS()],
+  plugins: [
+    VueRouter({
+      /* options */
+    }),
+    AutoImport({
+      /* options */
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        {
+          // add any other imports you were relying on
+          'vue-router/auto': ['useLink']
+        },
+        '@vueuse/core'
+      ]
+    }),
+    vue(),
+    vueJsx(),
+    UnoCSS()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
