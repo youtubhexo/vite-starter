a1f667ea236365df3780dc14d981b69553365a1b
张王岩 <youtubhexo@gmail.com>
2024-03-02 09:18:52 +0800

    自动组件注册：  unplugin-vue-components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,11 +4,13 @@
       Hello world by vite-plugin-pages
     </span>
     <div>Now: {{ now }}</div>
+    <HelloWorld></HelloWorld>
 
     <!-- //<img src="https://desperado.cloud/picgo-img//20240227135105.png"> -->
   </div>
 </template>
 <script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
 import { useNow } from '@vueuse/core'
 
 const now = useNow()
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import UnoCSS from 'unocss/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -35,6 +36,9 @@ export default defineConfig({
         },
         '@vueuse/core'
       ]
+    }),
+    Components({
+      /* options */
     }),
     vue(),
     vueJsx(),
