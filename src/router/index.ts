4562e3484f7fd4b4569994c86979cd8b1f5a0d0e
张王岩 <youtubhexo@gmail.com>
2024-06-11 15:16:06 +0800

    前端路由的两种解决方案  & Better Comments -vscode插件安装

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // ?前端路由的两种解决方案
+  //  #/about  hash模式
+  //  /about   webhistory模式
+  //  SEO   ssr   //?Nuxt.js  描述: 一个基于 Vue.js 的框架，提供了 SSR 和静态站点生成功能。
   routes: [
     {
       path: '/',
